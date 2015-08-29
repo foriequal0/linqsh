e6bbcbe41f72590e0d0bc83f7fe850fbbab20276
SeongChan Lee <foriequal@gmail.com>
2015-08-30 08:33:33 +0900

    first version of filter

--- linqsh/cmds/filter.py
+++ linqsh/cmds/filter.py
@@ -1,15 +1,27 @@
+import sys
+import subprocess
+import argparse
+import linqsh.env as env
+import linqsh.utils.split as split
+
 __author__ = 'foriequal0'
 
-import linqsh.env as env
 
 COMMAND='filter'
 
 
-def add_arguments(parser):
+def add_arguments(parser: argparse.ArgumentParser):
+    parser.add_argument('command',
+                       help='ex: [ $1 == "asdf" ]')
     pass
 
 
 def cmd_main(args):
-    pass
+    for record in split.iter_stream(sys.stdin):
+        fields = split.field_split(record)
 
-
+        p = subprocess.Popen([env.SHELL, '-c', args.command, record])
+        if p.returncode == 0:
+            print(record)
+        else:
+            continue
